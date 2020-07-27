e775e3e81a6d3a41ab3a6396733a42fb4c72538a
Tibs7777 <tom.bartlett2@btinternet.com>
2020-07-27 15:14:28 +0100

    first commit

---
+++ precache-manifest.d2432093179329c96fc4f514ea4cf27c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f80653dfd495673cc6769370cf8db7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d94bb0530efcfe17c4d0",
+    "url": "/static/css/main.97517292.chunk.css"
+  },
+  {
+    "revision": "02f0496e904c6c5a90bf",
+    "url": "/static/js/2.ff8adfc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff8adfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94bb0530efcfe17c4d0",
+    "url": "/static/js/main.6d481ca7.chunk.js"
+  },
+  {
+    "revision": "fbdc155f07cd5f39f453",
+    "url": "/static/js/runtime-main.c17b85a7.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
